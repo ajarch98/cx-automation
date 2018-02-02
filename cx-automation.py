5e3dc5d848708a3778312699620bbc793bae7fea
ajarch98 <35962651+ajarch98@users.noreply.github.com>
2018-02-02 18:31:35 +0300

    Update cx-automation.py

--- cx-automation.py
+++ cx-automation.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 chromedriver_path=r'C:\Users\Advait Joshi\Documents\Python\automation\chromedriver.exe'
 website_path=r'https://cx.usiu.ac.ke/ICS/'
@@ -28,6 +29,10 @@ check_grades_elem.click()
 #check grades page
 view_report_elem=driver.find_element_by_link_text('View Final Grade Report')
 view_report_elem.click()
+
+#scroll down on grades page
+html_elem=driver.find_element_by_tag_name('html')
+html_elem.send_keys(Keys.PAGE_DOWN)
 
 time.sleep(5)
 driver.quit()
